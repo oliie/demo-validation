0dcd5b9a0350bf37ef199832a42371cba9c1d227
Oliver Praesto <oliver.praesto@gmail.com>
2024-03-20 13:27:26 +0100

    - adds manual form validation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,53 @@
 <script lang="ts">
 	import '@picocss/pico/css/pico.pumpkin.min.css';
+
+	let payload = {};
+
+	let name = '';
+	let email = '';
+	let password = '';
+	let passwordValidation = '';
+
+	let errorMessage = '';
+
+	function isGeneralStringValid(value: string) {
+		return value && value.length > 0;
+	}
+
+	function isEmailValid(value: string) {
+		const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+		return value && regex.test(value);
+	}
+
+	function isPasswordValid(value: string) {
+		return value && value.length >= 8;
+	}
+
+	function isPasswordValidationValid(value: string) {
+		return value && value === password;
+	}
+
+	function handleSubmit() {
+		const validForm =
+			isGeneralStringValid(name) &&
+			isEmailValid(email) &&
+			isPasswordValid(password) &&
+			isPasswordValidationValid(passwordValidation);
+
+		if (!validForm) {
+			errorMessage = 'Formuläret är inte korrekt ifyllt! 😭';
+			throw new Error(errorMessage);
+		}
+
+		errorMessage = '';
+		payload = {
+			name,
+			email,
+			password,
+			passwordValidation
+		};
+	}
 </script>
 
 <header>
@@ -7,15 +55,16 @@
 </header>
 
 <main class="container">
-	<form class="user-form" action="">
-		<input type="text" name="name" id="name" placeholder="Namn" aria-invalid="false" />
+	<form class="user-form" on:submit|preventDefault={handleSubmit}>
+		<input type="text" bind:value={name} name="name" placeholder="Namn" aria-invalid="false" />
 
-		<input type="email" name="email" id="email" placeholder="E-post" />
+		<input type="email" bind:value={email} name="email" placeholder="E-post" />
 
-		<input type="text" name="password" id="password" placeholder="Lösenord" />
+		<input type="text" bind:value={password} name="password" placeholder="Lösenord" />
 
 		<input
 			type="text"
+			bind:value={passwordValidation}
 			name="password-validation"
 			id="password-validation"
 			placeholder="Lösenord igen"
@@ -23,6 +72,12 @@
 
 		<button type="submit">Skapa användare</button>
 	</form>
+
+	{#if errorMessage}
+		<p>{errorMessage}</p>
+	{:else}
+		{JSON.stringify(payload)}
+	{/if}
 </main>
 
 <style>
