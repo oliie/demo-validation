290589deefd2e664d8b8166b125a62c53664f461
Oliver Praesto <oliver.praesto@gmail.com>
2024-03-20 12:00:52 +0100

    - adds boilerplate with pico

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import '@picocss/pico/css/pico.pumpkin.min.css';
+</script>
+
+<main class="container">
+	<form class="user-form" action="">
+		<input type="text" name="name" id="name" placeholder="Namn" aria-invalid="false" />
+
+		<input type="email" name="email" id="email" placeholder="E-post" />
+
+		<input type="text" name="password" id="password" placeholder="Lösenord" />
+
+		<input
+			type="text"
+			name="password-validation"
+			id="password-validation"
+			placeholder="Lösenord igen"
+		/>
+
+		<button type="submit">Skapa användare</button>
+	</form>
+</main>
+
+<style>
+	main {
+		margin-top: 5rem;
+	}
+</style>
