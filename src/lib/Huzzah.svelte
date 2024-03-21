c9fafda6cd986821dd7bd90bfac1ddfd67c2af15
Oliver Praesto <oliver.praesto@gmail.com>
2024-03-21 15:52:40 +0100

    - fixes confetti

--- src/lib/Huzzah.svelte
+++ src/lib/Huzzah.svelte
@@ -6,10 +6,10 @@
 
 {#if success}
 	<Confetti
-		x={[-2, 2]}
-		y={[0, 2]}
+		x={[-3, 3]}
+		y={[-0.5, 2]}
 		amount={100}
-		size={30}
+		size={50}
 		colorArray={['url(https://pixelcode.se/wp-content/uploads/2023/03/favicon_pixelcode_1.png)']}
 	/>
 {/if}
